0590017e2d23e65b30879e2fb97631644203a6cb
Maryam Gohargani <maryam.gohargani@mail.utoronto.ca>
2020-07-01 23:43:18 -0400

    finished MainController

--- phase1/src/controllers/MainController.java
+++ phase1/src/controllers/MainController.java
@@ -3,14 +3,15 @@ package controllers;
 import gateways.ManualConfig;
 
 /**
- * A class for the main logic of the program that puts controllers together.
+ * A class that initiates the home page.
  */
 public class MainController {
     /**
-     * Starts the program and initiates ManualConfig and Controllers.
+     * Starts the program and initiates ManualConfig and AuthController.
      */
     public void run() {
         ManualConfig manualConfig = new ManualConfig();
-        // TODO waiting for controllers to be done so i can put the logic together.
+        AuthController authController = new AuthController(manualConfig);
+        authController.run();
     }
 }
