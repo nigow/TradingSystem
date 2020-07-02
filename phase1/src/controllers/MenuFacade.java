437f041e62b1d90192aeb585a583760db9350b8e
Maryam Gohargani <maryam.gohargani@mail.utoronto.ca>
2020-07-01 23:35:40 -0400

    added some code to MenuFacade

--- phase1/src/controllers/MenuFacade.java
+++ phase1/src/controllers/MenuFacade.java
@@ -1,22 +1,78 @@
 package controllers;
 
 import gateways.ManualConfig;
+import presenters.ConsoleMenuPresenter;
+import presenters.MenuPresenter;
+import usecases.AuthManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A class that manages a user's options according to their permissions.
  * @author Maryam
  */
 public class MenuFacade {
+
     /**
-     * Initializes MenuFacade based on information from ManualConfig and creates instances of // TODO
+     * An instance of AuthManager to access a user's permissions.
+     */
+    private AuthManager authManager;
+
+    /**
+     * An instance of MenuPresenter to display options.
+     */
+    private MenuPresenter menuPresenter;
+
+    /**
+     * Initializes MenuFacade based on information from ManualConfig and creates instances of
+     * AuthManager and necessary controllers.
      * @param mc An instance of ManualConfig
      */
     public MenuFacade(ManualConfig mc) {
+        authManager = mc.getAuthManager();
+        menuPresenter = new ConsoleMenuPresenter();
     }
 
     /**
-     * Calls the presenter with options for the user based on their permission.
+     * Calls the presenter with options for the user based on their permission and executes the action.
      */
     public void run() {
+        List<String> options = new ArrayList<>();
+        List<Runnable> method = new ArrayList<>();
+
+        /*
+        add options and which controller 'run' should be called based on that
+        LOGIN,
+        FREEZE,
+        UNFREEZE,
+        CREATE_ITEM,
+        CONFIRM_ITEM,
+        ADD_TO_WISHLIST,
+        LEND,
+        BORROW,
+        BROWSE_INVENTORY,
+        CHANGE_RESTRICTIONS,
+        ADD_ADMIN,
+        REQUEST_UNFREEZE
+        */
+
+        // TODO waiting on all controllers
+
+        method.add(() -> {});
+
+        String action = menuPresenter.displayMenu(options);
+        try {
+            int i = Integer.parseInt(action);
+            if (0 <= i && i < method.size()) {
+                method.get(i).run();
+            } else {
+                menuPresenter.invalidInput();
+                run();
+            }
+        } catch (NumberFormatException e) {
+            menuPresenter.invalidInput();
+            run();
+        }
     }
 }
--- phase1/src/controllers/TradeController.java
+++ phase1/src/controllers/TradeController.java
@@ -1,9 +1,23 @@
 package controllers;
 
 import gateways.ManualConfig;
+import presenters.ConsoleTradePresenter;
+import presenters.TradePresenter;
 
+// TODO: variable and method javadoc
+
+/**
+ * Controller that deals with user's past trades and making changes to them.
+ * @author Maryam
+ */
 public class TradeController {
+    private TradePresenter tradePresenter;
+
     public TradeController(ManualConfig mc) {
+        tradePresenter = new ConsoleTradePresenter();
+    }
+
+    public void run() {
 
     }
 }
