d7c5507eb39f200d36daa4c91bacd084ca64da90
mio <knightopress@gmail.com>
2020-07-03 20:55:58 -0400

    Use consistent directory naming scheme.

--- phase1/src/gateways/ManualConfigCSV.java
+++ phase1/src/gateways/ManualConfigCSV.java
@@ -24,7 +24,7 @@ public class ManualConfigCSV implements ManualConfig {
      * using existing CSV file.
      */
     public ManualConfigCSV() throws IOException {
-        String filePath = System.getProperty("user.dir") + "/out/Files/";
+        String filePath = System.getProperty("user.dir") + "/out/files/";
         initializeUseCases(filePath);
     }
 
