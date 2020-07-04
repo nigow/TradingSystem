5304f787fed616eeed8acdb7851e317021ff5293
xiaca <catherine.xia@mail.utoronto.ca>
2020-07-03 23:43:10 -0400

    tentative admin and admin controllers

--- phase1/src/controllers/AdminCreator.java
+++ phase1/src/controllers/AdminCreator.java
@@ -1,5 +1,34 @@
 package controllers;
 
+import gateways.ManualConfig;
+import presenters.AdminPresenter;
+import usecases.AccountManager;
+
 public class AdminCreator {
+
+    private AdminPresenter adminPresenter;
+
+    private AccountManager accountManager;
+
+    private ManualConfig mc;
+
+    public AdminCreator(ManualConfig mc, AdminPresenter adminPresenter) {
+        this.mc = mc;
+        accountManager = mc.getAccountManager();
+        this.adminPresenter = adminPresenter;
+    }
+
+    public void run() {
+        boolean isValid = false;
+        String[] info;
+        while (!isValid) {
+            info = adminPresenter.createAdmin();
+            isValid = accountManager.createAdminAccount(info[0], info[1]);
+            if (!isValid) {
+                adminPresenter.invalidInput();
+            }
+        }
+        //TODO returns user to main menu
+    }
 
 }
