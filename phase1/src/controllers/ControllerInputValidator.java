e85977f59d041a28672019b95642ff1c37ba56ed
mio <knightopress@gmail.com>
2020-07-05 00:49:32 -0400

    Add strict date validation to ControllerInputValidator (who said I don't like more work).

--- phase1/src/controllers/ControllerInputValidator.java
+++ phase1/src/controllers/ControllerInputValidator.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.time.Month;
+import java.time.Year;
 import java.util.regex.Pattern;
 
 /**
@@ -19,14 +21,23 @@ public class ControllerInputValidator {
     }
 
     /**
-     * Check if a string is a date in the form of yyyy-mm-dd.
+     * Check if a string is a valid date in the form of yyyy-mm-dd.
      * @param input Input string.
-     * @return Whether given string is a date in the form of yyyy-mm-dd.
+     * @return Whether given string is a valid date in the form of yyyy-mm-dd.
      */
     public boolean isDate(String input) {
 
         // citation: https://stackoverflow.com/questions/22061723/regex-date-validation-for-yyyy-mm-dd
-        return Pattern.matches("^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", input);
+        if (Pattern.matches("^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", input)) {
+
+            int year = Integer.parseInt(input.split("-")[0]);
+            int month = Integer.parseInt(input.split("-")[1]);
+            int day = Integer.parseInt(input.split("-")[2]);
+
+            return day <= Month.of(month).length(Year.isLeap(year));
+
+        }
+        return false;
 
     }
 
