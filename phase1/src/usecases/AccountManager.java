2802ff910ecced0dd7b052da461dea2bbf0aae53
Andrew Li <longyu.li@mail.utoronto.ca>
2020-07-05 20:37:39 -0400

    simplified method

--- phase1/src/usecases/AccountManager.java
+++ phase1/src/usecases/AccountManager.java
@@ -74,11 +74,8 @@ public class AccountManager {
      * @return Whether an account can be created
      */
     private boolean validateAccountRegister(String username, String password) {
-        if(username.matches("^[a-zA-Z0-9_]*$") && password.matches("^[ -~]*$")
-                && !password.contains(",") && accountGateway.findByUsername(username) == null){
-            return true;
-        }
-        return false;
+        return username.matches("^[a-zA-Z0-9_]*$") && password.matches("^[ -~]*$") &&
+                !password.contains(",") && accountGateway.findByUsername(username) == null;
     }
 
     /**
