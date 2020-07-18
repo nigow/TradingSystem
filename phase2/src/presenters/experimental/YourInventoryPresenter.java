f9ea517a9c66f18c4b228274f3f137ca9257fbd7
Ethan Lam <mailing.ethanlam@gmail.com>
2020-07-17 21:23:18 -0400

    pushed update to YourInventoryController, and created a dum dum YourInventoryPresenter for now

--- phase2/src/presenters/experimental/YourInventoryPresenter.java
+++ phase2/src/presenters/experimental/YourInventoryPresenter.java
@@ -1,4 +1,17 @@
 package presenters.experimental;
 
+
+import java.util.List;
+
+/**
+ * Presenter for displaying your inventory
+ *
+ * @author Ethan (follow @ethannomiddlenamelam on instagram)
+ */
 public class YourInventoryPresenter {
+
+    public void displayInventory(List<String> items) {
+        //TODO: figure out how to make the view observe this.
+    }
+
 }
