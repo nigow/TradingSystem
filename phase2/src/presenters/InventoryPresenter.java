9829890c8c11cfc5008790b0bbfa876b14429b7e
xiaca <catherine.xia@mail.utoronto.ca>
2020-07-21 21:45:03 -0400

    eliminate all strings from inventory controllers/presenters

--- phase2/src/presenters/InventoryPresenter.java
+++ phase2/src/presenters/InventoryPresenter.java
@@ -71,34 +71,145 @@ public interface InventoryPresenter {
      * Tells user that they've successfully aborted their choice.
      */
     void abortMessage();
-    
+
+    /**
+     * Displays all approved items.
+     * @param approvedItems list of approved items
+     */
     void displayApprovedItems(List<String> approvedItems);
 
+    /**
+     * Displays all items, including pending items.
+     * @param allItems list of all items
+     */
     void displayAllItems(List<String> allItems);
 
+    /**
+     * Displays the pending items.
+     * @param pendingItems list of pending items
+     */
     void displayUserPendingItems(List<String> pendingItems);
 
+    /**
+     * Displays items available to trade
+     * @param availableItems list of available items
+     */
     void displayAvailableItems(List<String> availableItems);
 
+    /**
+     * Displays all pending items.
+     * @param pendingItems list of pending items
+     */
     void displayAllPendingItems(List<String> pendingItems);
 
+    /**
+     * Displays all items belonging to other users
+     * @param othersItems list of items from other users
+     */
     void displayOthersItems(List<String> othersItems);
 
+    /**
+     * Displays all of the user's items.
+     * @param userItems list of user's items
+     */
     void displayUserItems(List<String> userItems);
 
+    /**
+     * Tells user the input does not correspond to any option.
+     */
     void displayDoesNotCorrespond();
 
+    /**
+     * Tells user they're not allowed to include commas in the input.
+     */
     void commaError();
 
+    /**
+     * Tells user that an item was not successfully added.
+     */
     void itemError();
 
+    /**
+     * Tells user that an item was successfully added.
+     */
     void itemSuccess();
 
+    /**
+     * Tells user that an item is pending approval.
+     */
     void pending();
 
+    /**
+     * Tells user that an item was successfully removed.
+     */
     void itemRemovalSuccess();
 
+    /**
+     * Tells user that an item was successfully removed.
+     */
     void itemRemovalError();
 
+    /**
+     * Tells user that an item has been approved by an admin.
+     */
     void itemApproved();
+
+    /**
+     * Shows option to view all approved items.
+     * @return the string message
+     */
+    String viewAllApproved();
+
+    /**
+     * Shows option to view user's approved items.
+     * @return the string message
+     */
+    String viewYourApproved();
+    /**
+     * Shows option to view user's approved items.
+     * @return the string message
+     */
+    String viewYourPending();
+
+    /**
+     * Shows option to view all tradable items.
+     * @return the string message
+     */
+    String viewAllAvailable();
+
+    /**
+     * Shows option to add an item to the wishlist.
+     * @return the string message
+     */
+    String addItemToWishlist();
+
+    /**
+     * Shows option to create a new item.
+     * @return the string message
+     */
+    String createNewItem();
+
+    /**
+     * Shows option to view all pending items.
+     * @return the string message
+     */
+    String viewAllPending();
+
+    /**
+     * Shows option to view approve an item.
+     * @return the string message
+     */
+    String approveAnItem();
+
+    /**
+     * Shows option to remove an item from the wishlist.
+     * @return the string message
+     */
+    String removeYourItem();
+
+    /**
+     * Shows option to return to home.
+     * @return the string message
+     */
+    String returnToHome();
 }
