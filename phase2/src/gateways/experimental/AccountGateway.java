57f2c643bd59055a05e9eb830ab4e64be8ab74b1
mio <knightopress@gmail.com>
2020-07-25 21:56:08 -0400

    More gateway migration work.

--- phase2/src/gateways/experimental/AccountGateway.java
+++ phase2/src/gateways/experimental/AccountGateway.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface AccountGateway {
 
     void populate(AccountRepository accountRepository);
-    void save(String username, String password, List<Integer> wishlist, List<String> permissions, int accountID);
 
+    // account_id,username,password,wishlist,permissions
+    void save(int accountId, String username, String password, List<Integer> wishlist, List<String> permissions);
 }
