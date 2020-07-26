fb93280d02699b189876e568e3424dc9d1b0459f
Andrew Li <longyu.li@mail.utoronto.ca>
2020-07-26 02:18:40 -0400

    added todo and changed to need list of Permissions instead of list of strings

--- phase2/src/gateways/experimental/AccountGateway.java
+++ phase2/src/gateways/experimental/AccountGateway.java
@@ -1,7 +1,9 @@
 package gateways.experimental;
 
+import entities.Permissions;
 import usecases.AccountRepository;
 
+import java.security.Permission;
 import java.util.List;
 
 public interface AccountGateway {
@@ -9,5 +11,6 @@ public interface AccountGateway {
     void populate(AccountRepository accountRepository);
 
     // account_id,username,password,wishlist,permissions
-    void save(int accountId, String username, String password, List<Integer> wishlist, List<String> permissions);
+    //TODO ask Lindsey if controllers/gateways can interact with enums(I think enums aren't entities)
+    void save(int accountId, String username, String password, List<Integer> wishlist, List<Permissions> permissions);
 }
