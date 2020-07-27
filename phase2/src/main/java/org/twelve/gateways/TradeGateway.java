b6708819c26b8b9985af53ee0373a67f8578f298
Tairi <tairi@kotozna.com>
2020-07-28 02:17:35 +0900

    changed populate method parameter from TradeUtility to TradeManager

--- phase2/src/main/java/org/twelve/gateways/TradeGateway.java
+++ phase2/src/main/java/org/twelve/gateways/TradeGateway.java
@@ -1,12 +1,13 @@
 package org.twelve.gateways;
 
+import org.twelve.usecases.TradeManager;
 import org.twelve.usecases.TradeUtility;
 
 import java.util.List;
 
 public interface TradeGateway {
 
-    void populate(TradeUtility tradeUtility);
+    void populate(TradeManager tradeManager);
 
     // needs storage spec update
     void save(int tradeId, boolean isPermanent, List<Integer> traderIds, List<Integer> itemIds, int editedCounter,
