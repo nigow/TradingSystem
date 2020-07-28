294e499f8ad347cc65b48243d0e9e76d5f9cbb7b
Isaac <895summernorth@gmail.com>
2020-07-28 19:53:35 -0400

    Added save method to InMemoryTradeGateway and InMemoryItemGateway

--- phase2/src/test/java/org/twelve/gateways/InMemoryItemGateway.java
+++ phase2/src/test/java/org/twelve/gateways/InMemoryItemGateway.java
@@ -1,8 +1,17 @@
 package test.java.org.twelve.gateways;
 
+import org.twelve.entities.Item;
 import org.twelve.usecases.ItemManager;
 
+import java.util.*;
+
 public class InMemoryItemGateway implements org.twelve.gateways.ItemsGateway {
+
+    public final Map<Integer, org.twelve.entities.Item> itemMap;
+
+    public InMemoryItemGateway(Map<Integer, org.twelve.entities.Item> itemMap) {
+        this.itemMap = itemMap;
+    }
 
     @Override
     public void populate(ItemManager itemManager) {
@@ -11,6 +20,10 @@ public class InMemoryItemGateway implements org.twelve.gateways.ItemsGateway {
 
     @Override
     public void save(int itemId, String name, String description, boolean isApproved, int ownerId) {
-
+        org.twelve.entities.Item item = new Item(itemId, name, description, ownerId);
+        if (isApproved) {
+            item.approve();
+        }
+        itemMap.put(item.getItemID(), item);
     }
 }
--- phase2/src/test/java/org/twelve/gateways/InMemoryTradeGateway.java
+++ phase2/src/test/java/org/twelve/gateways/InMemoryTradeGateway.java
@@ -1,17 +1,38 @@
 package test.java.org.twelve.gateways;
 
+import org.twelve.entities.TimePlace;
+import org.twelve.entities.Trade;
 import org.twelve.usecases.TradeManager;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryTradeGateway implements org.twelve.gateways.TradeGateway {
+
+    public final Map<Integer, org.twelve.entities.Trade> tradeMap;
+    public final Map<Integer, org.twelve.entities.TimePlace> timePlaceMap;
+
+    public InMemoryTradeGateway(Map<Integer, org.twelve.entities.Trade> trades,
+                                Map<Integer, org.twelve.entities.TimePlace> timePlace) {
+        this.timePlaceMap = timePlace;
+        this.tradeMap = trades;
+    }
+
     @Override
     public void populate(TradeManager tradeManager) {
 
     }
 
     @Override
-    public void save(int tradeId, boolean isPermanent, List<Integer> traderIds, List<Integer> itemIds, int editedCounter, String tradeStatus, List<Boolean> tradeCompletions, String time, String location) {
-
+    public void save(int tradeId, boolean isPermanent, List<Integer> traderIds, List<Integer> itemIds,
+                     int editedCounter, String tradeStatus, List<Boolean> tradeCompletions, String time,
+                     String location) {
+        org.twelve.entities.Trade trade = new Trade(tradeId, isPermanent, traderIds,
+                itemIds, editedCounter, org.twelve.entities.TradeStatus.valueOf(org.twelve.entities.TradeStatus),
+                tradeCompletions);
+        org.twelve.entities.TimePlace timePlace = new TimePlace(tradeId, LocalDateTime.parse(time), location);
+        tradeMap.put(tradeId, trade);
+        timePlaceMap.put(tradeId, timePlace);
     }
 }
