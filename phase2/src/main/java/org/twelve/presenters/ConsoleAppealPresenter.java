973dfd684b19ef8edfd6a96e71b7b4e027706753
Andrew Li <longyu.li@mail.utoronto.ca>
2020-07-30 13:45:22 -0400

    updated to support vacations

--- phase2/src/main/java/org/twelve/presenters/ConsoleAppealPresenter.java
+++ phase2/src/main/java/org/twelve/presenters/ConsoleAppealPresenter.java
@@ -4,11 +4,33 @@ package org.twelve.presenters;
  * Console presenter for letting user appeal to be unfrozen.
  */
 public class ConsoleAppealPresenter implements AppealPresenter {
+
+
     /**
      * {@inheritDoc}
      */
     @Override
-    public void displaySuccessfulAppeal(String username) {
+    public void displayUnfreezeAppeal(String username) {
         System.out.println(username + "'s appeal has been received.");
     }
+
+    @Override
+    public void displayVacationAppeal(String username) {
+
+    }
+
+    @Override
+    public void displayVacationCompletion(String username) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void invalidInput() {
+        System.out.println("Your input was invalid. Please try again.");
+    }
+
+
 }
