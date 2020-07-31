ce78bf96f7e1435b4363e5a43c029c2c96f2a41a
Isaac <895summernorth@gmail.com>
2020-07-30 21:50:00 -0400

    Fixed ItemIntegrationTest

---
+++ phase2/src/test/java/org/twelve/usecases/TradeIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.twelve.usecases;
+
+import junit.framework.TestCase;
+import org.twelve.gateways.TradeGateway;
+import org.twelve.usecases.TradeManager;
+
+public class TradeIntegrationTest extends TestCase {
+    private TradeManager tradeManager;
+    private TradeGateway tradeGateway;
+
+
+}
