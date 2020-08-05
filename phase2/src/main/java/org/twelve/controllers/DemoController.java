390bfb46dd17beb27035cec7863651b02d8d13fe
Maryam Gohargani <maryam.gohargani@mail.utoronto.ca>
2020-08-05 11:08:57 -0400

    setting thresholds loose in demo controller

--- phase2/src/main/java/org/twelve/controllers/DemoController.java
+++ phase2/src/main/java/org/twelve/controllers/DemoController.java
@@ -5,6 +5,7 @@ package org.twelve.controllers;
 import org.twelve.entities.Permissions;
 import org.twelve.usecases.AccountRepository;
 import org.twelve.usecases.SessionManager;
+import org.twelve.usecases.ThresholdRepository;
 import org.twelve.usecases.UseCasePool;
 
 import java.util.Arrays;
@@ -19,10 +20,12 @@ public class DemoController {
 
     private final AccountRepository accountRepository;
     private final SessionManager sessionManager;
+    private final ThresholdRepository thresholdRepository;
 
     public DemoController(UseCasePool useCasePool) {
         accountRepository = useCasePool.getAccountRepository();
         sessionManager = useCasePool.getSessionManager();
+        thresholdRepository = useCasePool.getThresholdRepository();
     }
 
     public void createDemoAccount() {
@@ -33,5 +36,16 @@ public class DemoController {
         while (!accountRepository.createAccount("demo" + counter, "12345", perms, "UTM"))
             counter++;
         sessionManager.login("demo" + counter);
+
+        // TODO rebuild gateways to in-memory gateways
+
+        // this loosens the regular threshold values of our program, so that the
+        //  demo user can borrow/lend/trade without any restrictions and without
+        //  getting frozen.
+        thresholdRepository.setLendMoreThanBorrow(-1000);
+        thresholdRepository.setMaxIncompleteTrade(1000);
+        thresholdRepository.setMaxWeeklyTrade(1000);
+        thresholdRepository.setNumberOfEdits(100);
+        thresholdRepository.setRequiredTradesForTrusted(10);
     }
 }
