80a58d3304a1dc167df17800404b4f4d6d5cd679
Andrew Li <longyu.li@mail.utoronto.ca>
2020-08-13 17:55:32 -0400

    created and implemented presenters for TradeCancellation

---
+++ phase2/src/main/java/org/twelve/presenters/ui/UITradeCancellationPresenter.java
@@ -0,0 +1,31 @@
+package org.twelve.presenters.ui;
+
+import org.twelve.presenters.TradeCancellationPresenter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class UITradeCancellationPresenter extends ObservablePresenter implements TradeCancellationPresenter {
+
+    private List<String> allTrades;
+    private final ResourceBundle localizedResources;
+
+    public UITradeCancellationPresenter(ResourceBundle localizedResources) {
+        super();
+        this.localizedResources = localizedResources;
+        setAllTrades(new ArrayList<>());
+    }
+
+    @Override
+    public List<String> getAllTrades() {
+        return allTrades;
+    }
+
+    @Override
+    public void setAllTrades(List<String> allTrades) {
+        List <String> oldTrades = this.allTrades;
+        this.allTrades = allTrades;
+        propertyChangeSupport.firePropertyChange("peerWishlist", oldTrades, allTrades);
+    }
+}
