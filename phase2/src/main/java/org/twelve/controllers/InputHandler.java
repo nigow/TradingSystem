6e76156bc507e234e9b96c05853cad19c3540bbc
Michael Sheinman Orenstrakh <michael092001@gmail.com>
2020-08-15 20:53:30 -0400

    Removed unused InputHandler methods.

--- phase2/src/main/java/org/twelve/controllers/InputHandler.java
+++ phase2/src/main/java/org/twelve/controllers/InputHandler.java
@@ -8,52 +8,6 @@ import java.util.regex.Pattern;
  * Handler that contains helper methods for verifying input.
  */
 public class InputHandler {
-    /**
-     * Check if a string is a non-negative integer.
-     *
-     * @param input Input string
-     * @return Whether given string is a non-negative integer
-     */
-    public boolean isNum(String input) {
-
-        return Pattern.matches("^[0-9]+$", input);
-
-    }
-
-    /**
-     * Check if a string is a valid date in the form of yyyy-mm-dd.
-     *
-     * @param input Input string
-     * @return Whether given string is a valid date in the form of yyyy-mm-dd
-     */
-    public boolean isDate(String input) {
-
-        // citation: https://stackoverflow.com/questions/22061723/regex-date-validation-for-yyyy-mm-dd
-        if (Pattern.matches("^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$", input)) {
-
-            int year = Integer.parseInt(input.split("-")[0]);
-            int month = Integer.parseInt(input.split("-")[1]);
-            int day = Integer.parseInt(input.split("-")[2]);
-
-            return day <= Month.of(month).length(Year.isLeap(year));
-
-        }
-        return false;
-
-    }
-
-    /**
-     * Check if a string is a time in the form of hh:mm.
-     *
-     * @param input Input string
-     * @return Whether given string is a time in the form of hh:mm
-     */
-    public boolean isTime(String input) {
-
-        // citation: https://stackoverflow.com/questions/7536755/regular-expression-for-matching-hhmm-time-format
-        return Pattern.matches("^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$", input);
-
-    }
 
     /**
      * Determines if username contains only acceptable characters.
